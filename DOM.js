5d2765a4b5602d937feb731e603f002da5596df1
Lanre <olawunmilanre00@gmail.com>
2023-07-28 17:54:25 +0100

    update

---
+++ DOM.js
@@ -0,0 +1,19 @@
+let iconElement = document.getElementById('iconHeart');
+
+function changeIconColor() {
+  iconElement.style.color = 'red';
+
+}
+
+// Attach click event to the icon
+
+iconElement.addEventListener('click', changeIconColor);
+  
+let iconElemente = document.getElementById('iconHeart1');
+function changeIconColorr() {
+  iconElemente.style.color = 'red';
+
+}
+
+  // Attach click event to the icon
+  iconElemente.addEventListener('click', changeIconColorr);
